7b8e4ecdd69222c5ad6fb295e106ee5287d6f642
saba0308 <sabapathi030820@gmail.com>
2023-03-02 11:29:57 +0530

    first commit

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,19 +1,19 @@
 import { Card, CardContent, Stack, Typography } from "@mui/material";
 
 import React, { Fragment } from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+
 import * as Yup from 'yup';
 import './Contact.css'
 import {
   Paper,
   Box,
-  Grid,
-  TextField,
-  Button,
+  Grid
+
 } from '@mui/material';
+import { useFormik } from 'formik';
 const Contact = () => {
   const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
   const validationSchema = Yup.object().shape({
     fullname: Yup.string().required('Name is required'),
     mobilenumber: Yup.string().required('Mobile Number is required').matches(phoneRegExp, 'Phone number is not valid'),
@@ -21,153 +21,193 @@ const Contact = () => {
     comments: Yup.string().required('Comments are required')
   });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(validationSchema),
+
+  const formik = useFormik({
+    initialValues: {
+      fullname: '',
+      mobilenumber: '',
+      email: '',
+      comments: ''
+    },
+    validationSchema,
+    // validateOnChange: false,
+    // validateOnBlur: false,
+    onSubmit: (data) => {
+      console.log(JSON.stringify(data, null, 2));
+    },
   });
 
-  const onSubmit = (data) => {
-    console.log(JSON.stringify(data, null, 2));
-  };
+
   return (
     <div className='homeContainer'>
       <Stack direction={"column"} spacing={4}>
-        
-     
-          <Stack direction={{ xs: 'column', sm: 'row' }}
-      spacing={{ xs: 1, sm: 2, md: 4 }} >
-            <Fragment>
-              <Paper sx={{ width:350,position:'relative',left:'50px' }}>
-              <Typography fontSize={42} textAlign='center' fontWeight={400} variant="h2" color='#1976d2;'>Contact Us</Typography>
-                <Box px={3} py={2}>
 
-                  <Grid container spacing={1}>
-                    <Grid item xs={12} sm={12}>
-                      <TextField
-                        required
-                        id="fullnames1111"
+
+        <Stack direction={{ xs: 'column', sm: 'row' }}
+          spacing={{ xs: 1, sm: 2, md: 4 }} >
+          <Fragment>
+            <Paper sx={{ width: 350, position: 'relative', left: '50px' }}>
+              <Typography fontSize={42} textAlign='center' fontWeight={400} variant="h2" color='#1976d2;'>Contact Us</Typography>
+              <Box px={3} py={2}>
+
+                <Grid container spacing={1}>
+                  <Grid item xs={12} sm={12}>
+                    <div className="form-group">
+                      <label htmlFor="fullname"> Name</label>
+                      <input
                         name="fullname"
-                        label="Name"
-                        fullWidth
-                        margin="dense"
-                        {...register('fullname111')}
-                        error={errors.fullname ? true : false}
+                        type="text"
+                        className={
+                          'form-control' +
+                          (formik.errors.fullname && formik.touched.fullname
+                            ? ' is-invalid'
+                            : '')
+                        }
+                        onChange={formik.handleChange}
+                        value={formik.values.fullname}
                       />
-                      <Typography variant="inherit" color="red">
-                        {errors.fullname?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                      <TextField
-                        required
-                        id="mobilenumber"
+                      <div className="invalid-feedback">
+                        {formik.errors.fullname && formik.touched.fullname
+                          ? formik.errors.fullname
+                          : null}
+                      </div>
+                    </div>
+
+
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <div className="form-group">
+                      <label htmlFor="mobilenumber"> Mobile Number </label>
+                      <input
                         name="mobilenumber"
-                        label="Mobile Number"
-                        fullWidth
-                        margin="dense"
-                        {...register('mobilenumber')}
-                        error={errors.mobilenumber ? true : false}
+                        type="text"
+                        className={
+                          'form-control' +
+                          (formik.errors.mobilenumber && formik.touched.mobilenumber
+                            ? ' is-invalid'
+                            : '')
+                        }
+                        onChange={formik.handleChange}
+                        value={formik.values.mobilenumber}
                       />
-                      <Typography variant="inherit" color="red">
-                        {errors.mobilenumber?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                      <TextField
-                        required
-                        id="email"
-                        name="email11111"
-                        label="Email"
-                        fullWidth
-                        margin="dense"
-                        error={errors.email ? true : false}
+                      <div className="invalid-feedback">
+                        {formik.errors.mobilenumber && formik.touched.mobilenumber
+                          ? formik.errors.mobilenumber
+                          : null}
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <div className="form-group">
+                      <label htmlFor="email"> Email </label>
+                      <input
+                        name="email"
+                        type="email"
+                        className={
+                          'form-control' +
+                          (formik.errors.email && formik.touched.email ? ' is-invalid' : '')
+                        }
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
                       />
-                      <Typography variant="inherit" color="red">
-                        {errors.email?.message}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={12}>
-                      <TextField
-                        required
-                        id="comments"
+                      <div className="invalid-feedback">
+                        {formik.errors.email && formik.touched.email
+                          ? formik.errors.email
+                          : null}
+                      </div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <div className="form-group">
+                      <label htmlFor="comments"> Comments</label>
+                      <textarea
                         name="comments"
-                        label="Comments"
-                        fullWidth
-                        margin="dense"
-                        {...register('comments')}
-                        error={errors.comments ? true : false}
-                      />
-                      <Typography variant="inherit" color="red">
-                        {errors.comments?.message}
-                      </Typography>
-                    </Grid>
-                  
+                        type="text"
+                        className={
+                          'form-control' +
+                          (formik.errors.comments && formik.touched.comments
+                            ? ' is-invalid'
+                            : '')
+                        }
+                        onChange={formik.handleChange}
+                        value={formik.values.comments}
+                        rows="4" cols="38"
+                      ></textarea>
+                      <div className="invalid-feedback">
+                        {formik.errors.comments && formik.touched.comments
+                          ? formik.errors.comments
+                          : null}
+                      </div>
+                    </div>
                   </Grid>
 
-                  <Box  mt={3} py={2}>
-                    <Button sx={{width:'100%'}}
-                      variant="contained"
-                      color="primary"
-                      onClick={handleSubmit(onSubmit)}
-                    >
-                      Register
-                    </Button>
-                  </Box>
-                </Box>
-              </Paper>
-            </Fragment>
-            <img className="contact" src="/images/Contact.png" alt="contact" />
-          </Stack>
-       
-   
-          <Stack sx={{position:'relative',left:'50px'}} direction={{ xs: 'column', sm: 'row' }}
-  spacing={{ xs: 1, sm: 2, md: 4 }} >
-            <Card sx={{ minWidth: 250 }}>
-              <CardContent>
+                </Grid>
 
-                <Typography variant="h5" color='#1976d2;' component="div">
-                  Development Center I
-                </Typography>
+                <div className="form-group">
+                  <button type="submit" className="btn btn-primary" onClick={formik.handleSubmit}>
+                    Register
+                  </button>
+                  {/* <button
+                    type="button"
+                    className="btn btn-warning float-right"
+                    onClick={formik.handleReset}
+                  >
+                    Reset
+                  </button> */}
+                </div>
+              </Box>
 
-                <Typography sx={{ textOverflow: 'ellipsis' }} variant="p">
-                  <br />
-                  # 21, First Floor, Rubic Complex,Chennai
-                </Typography>
-              </CardContent>
+            </Paper>
+          </Fragment>
+          <img className="contact" src="/images/Contact.png" alt="contact" />
+        </Stack>
 
-            </Card>
-            <Card sx={{ minWidth: 250 }}>
-              <CardContent>
 
-                <Typography color='#1976d2;' variant="h5" component="div">
-                  Development Center  II
-                </Typography>
+        <Stack sx={{ position: 'relative', left: '50px' }} direction={{ xs: 'column', sm: 'row' }}
+          spacing={{ xs: 1, sm: 2, md: 4 }} >
+          <Card sx={{ minWidth: 250 }}>
+            <CardContent>
 
-                <Typography sx={{ textOverflow: 'ellipsis' }} variant="address">
-                  <br />
-                  # 37, Third Floor, Rubic Complex,Bangalore
-                </Typography>
-              </CardContent>
+              <Typography variant="h5" color='#1976d2;' component="div">
+                Development Center I
+              </Typography>
 
-            </Card>
-            <Card sx={{ minWidth: 250 }}>
-              <CardContent>
+              <Typography sx={{ textOverflow: 'ellipsis' }} variant="p">
+                <br />
+                # 21, First Floor, Rubic Complex,Chennai
+              </Typography>
+            </CardContent>
 
-                <Typography color='#1976d2;' variant="h5" component="div">
-                  Head Office
-                </Typography>
+          </Card>
+          <Card sx={{ minWidth: 250 }}>
+            <CardContent>
 
-                <Typography sx={{ textOverflow: 'ellipsis' }} variant="address">
-                  <br />
-                  # 22, Second Floor, Rubic Complex,Bangalore
-                </Typography>
-              </CardContent>
+              <Typography color='#1976d2;' variant="h5" component="div">
+                Development Center  II
+              </Typography>
 
-            </Card>
-          </Stack>
+              <Typography sx={{ textOverflow: 'ellipsis' }} variant="address">
+                <br />
+                # 37, Third Floor, Rubic Complex,Bangalore
+              </Typography>
+            </CardContent>
+
+          </Card>
+          <Card sx={{ minWidth: 250 }}>
+            <CardContent>
+
+              <Typography color='#1976d2;' variant="h5" component="div">
+                Head Office
+              </Typography>
+
+              <Typography sx={{ textOverflow: 'ellipsis' }} variant="address">
+                <br />
+                # 22, Second Floor, Rubic Complex,Bangalore
+              </Typography>
+            </CardContent>
+
+          </Card>
+        </Stack>
 
 
       </Stack>
